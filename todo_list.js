a343ef1cc47ffc9fd398b2fcd9789b8ca58c9b0c
nikkig <nikkig@nikkigs-MacBook-Pro.local>
2022-02-22 12:40:53 -0800

    basic html and css

---
+++ todo_list.js
@@ -0,0 +1,25 @@
+/*
+problem: create a program that allows a user to enter todo items and delete them from a list.
+no login needed.
+list items are stored within the application itself. the app will provide a limit for number of list items allowed.
+
+classes: 
+List
+  has ListItems
+  has number of ListItems
+
+  createNewListItem
+  deleteListItem
+ListItem
+  has name
+  has dueDate
+  has details
+  has SubListItems
+  has number of SubListItems
+SubListItems
+  has name
+  has dueDate
+  has details
+  
+*/
+
